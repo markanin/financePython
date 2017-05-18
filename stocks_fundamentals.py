bdcef24d8c7c48984b7a1e9ac5862e71a974f54a
Maria Kaninia <maria.ev.kaninia@gmail.com>
2017-05-18 23:21:34 +0200

    Add files via upload
    Example of using Python module yahoo-finance to get stock fundamentals.

---
+++ stocks_fundamentals.py
@@ -0,0 +1,23 @@
+# Fundamentals of stocks using yahoo finance as data source
+
+from yahoo_finance import Share
+import time
+from datetime import date, timedelta
+import datetime as dt
+
+tickers = ['KMI', 'NNA', 'CAFD', 'GE', 'F', 'MMM', 'TXN', 'BBL', 'GLW', 'STM', 'INTC', 'QCOM']
+
+for ticker in tickers:
+    stockobj = Share(ticker)
+    price = stockobj.get_price()
+    bookvalue = stockobj.get_book_value()
+    p_e = stockobj.get_price_earnings_ratio()
+    market_cap = stockobj.get_market_cap()
+    price_to_book = float(price)/float(bookvalue)
+
+    print("\n%s" % ticker)
+    print("price %.2f" % float(price))
+    print("bookvalue %.2f" % float(bookvalue))
+    print("price_to_book %.2f" % float(price_to_book))   
+    print("P/E %.2f" % float(p_e))
+    print("market cap %s" % market_cap)
